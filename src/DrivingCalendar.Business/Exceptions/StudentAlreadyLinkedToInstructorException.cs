f096d159218babcad848ad91239c0140b152fe96
blondelina <adebrieta@gmail.com>
2023-01-04 18:11:54 +0200

    Spelling errors

--- src/DrivingCalendar.Business/Exceptions/StudentAlreadyLinkedToInstructorException.cs
+++ src/DrivingCalendar.Business/Exceptions/StudentAlreadyLinkedToInstructorException.cs
@@ -4,7 +4,7 @@ namespace DrivingCalendar.Business.Exceptions
 {
     internal class StudentAlreadyLinkedToInstructorException : CoreException
     {
-        private const string ERROR_MESSAGE = "Student is akready linked to instructor";
+        private const string ERROR_MESSAGE = "Student is already linked to instructor";
         public StudentAlreadyLinkedToInstructorException() : base(ERROR_MESSAGE, HttpStatusCode.BadRequest)
         {
         }
